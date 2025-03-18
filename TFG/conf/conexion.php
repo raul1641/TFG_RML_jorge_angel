3d99b76f315a01de93fe3443b48d054eba627484
raul1641 <147944096+raul1641@users.noreply.github.com>
2025-03-18 21:13:40 +0100

    Update conexion.php NOSE YO..

--- TFG/conf/conexion.php
+++ TFG/conf/conexion.php
@@ -1,48 +1,22 @@
-<?php
-// recogemos en unas variables los datos de la conexión
+$servidor = getenv('DB_HOST');
+$puerto = getenv('DB_PORT');
+$usuario = getenv('DB_USER');
+$contraseña = getenv('DB_PASSWORD');
+$shema = getenv('DB_NAME');
 
-// a "mysqli()" le tengo que pasar:
-// -> el servidor al que me quiero conectar
-// -> el usuario
-// -> la contraseña
-// -> esquema (shema) que quiero utilizar
+// Establecemos conexión al servidor de base de datos
+try {
+    $conexion = new mysqli($servidor, $usuario, $contraseña, $shema, $puerto);
 
-$servidor = "iasanz.synology.me:3306/rmolinal_PROYECTO_JORGE_ANGEL";
-$usuario="alumno";
-$contraseña="AlumnoSanz$1";
-$shema="rmolinal_PROYECTO_JORGE_ANGEL"
+    // Comprobamos si hay errores de conexión
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
 
-// $servidor="127.0.0.1";
-// $usuario="jorge";
-// $contraseña="666666.j";
-// $shema="registro_libros";
+    // Configurar la codificación UTF-8
+    $conexion->query("SET NAMES 'utf8'");
 
-//**********************************************************************
-// CONEXIÓN A LOCALHOST
-//**********************************************************************
-// si en el código que hay dentro de un "try" -> se produce un error->
-// se ejecuta el código que haya en el "catch"
-try
-{
-	// establecemos conexión al servidor de base de Datos
-	$conexion=new mysqli($servidor, $usuario, $contraseña, $shema);
-	
-	// visualizamos mensaje éxito
-	//echo "<font color='blue' size='5'>
-	//<b>MENSAJE:</b><br> La conexión (localhost) al Servidor de Base de Datos se ha establecido correctamente !!</font><br><br>";
-	
-	//para evitar problemas con acentos y ñ configuramos las querys de esta manera 
-	$conexion->query("SET NAMES 'utf8'");
-	
-} 
-//**********************************************************************
-// en "$e" tenemos la descripción del Error
-// podemos utilizarla o no utilizarla
-catch (exception $e)
-{
-    //echo "Error capturado: ".$e->getMessage()."<br>";
-	//echo "<font color='red' size='5'>
-	//<b>ERROR:</b><br> No se pudo realizar la conexión al Servidor de Base de Datos!!</font><br><br>";
+    echo "<b>MENSAJE:</b><br> Conexión exitosa a la base de datos.";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
-//**********************************************************************
-?>
